2fb405b417a32386fec9601265b5b3e95a389288
jethror1 <rainford1995@gmail.com>
2020-10-09 13:47:34 +0100

    fix panel naming

--- bin/coverage_report_single.py
+++ bin/coverage_report_single.py
@@ -137,11 +137,13 @@ class singleReport():
 
             # format according to output of
             # https://github.com/eastgenomics/eggd_generate_bed
-            panel_name = [x.strip("_") for x in panel_name.split("&&")]
-            panel_name = [x.replace("_", " ") for x in panel_name]
-            panel_name = [x.strip("_b37").strip("_b38") for x in panel_name]
+            panel_name = [x.strip("_") for x in panel_name.split("&&") if x]
+            panel_name = [
+                x.strip("_b37").strip("_b38") for x in panel_name if x
+            ]
+            panel_name = [x.replace("_", " ") for x in panel_name if x]
+            print(panel_name)
             panel_name = ",&nbsp".join(panel_name)
-
             panel = "<li>Panel(s) / gene(s) included in report: <b>{}</b>\
                 </li>".format(panel_name)
         else:
