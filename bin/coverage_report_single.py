6f9b770504a6d0de08facce327dfe6d7146258a4
jethror1 <rainford1995@gmail.com>
2020-11-30 09:37:38 +0000

    remove unused import

--- bin/coverage_report_single.py
+++ bin/coverage_report_single.py
@@ -19,7 +19,6 @@ import sys
 import matplotlib
 # use agg instead of tkinter for pyplot backend
 matplotlib.use('agg')
-# import matplotlib.image as mpimg
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import multiprocessing
