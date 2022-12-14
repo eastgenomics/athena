3cc2ef90518fa3d8cd938e202ec9b508160f92a5
Jethro Rainford <rainford1995@gmail.com>
2022-12-14 17:26:33 +0000

    fix overlap of x axis labels with legend

--- bin/coverage_report_single.py
+++ bin/coverage_report_single.py
@@ -340,7 +340,7 @@ class generatePlots():
             lambda x: f"{x[0]} ({x[1]})", axis=1
         )
 
-        summary_plot, axs = plt.subplots(figsize=(25, 7.5))
+        summary_plot, axs = plt.subplots(figsize=(25, 10))
 
         genes100pct = None
 
@@ -402,8 +402,8 @@ class generatePlots():
         red = mpatches.Patch(color='red', label='<90%')
 
         plt.legend(
-            handles=[green, orange, red], loc='upper center',
-            bbox_to_anchor=(0.5, -0.32),
+            handles=[green, orange, red], loc='lower center',
+            bbox_to_anchor=(0.5, -0.4),
             fancybox=True, shadow=True, ncol=12, fontsize=14
         )
 
@@ -416,7 +416,7 @@ class generatePlots():
             axs.set_xticks(axs.get_xticks()[::3])
             axs.tick_params(axis='both', which='major', labelsize=10)
             plt.figtext(
-                0.5, 0.1,
+                0.505, 0.01,
                 "Some gene labels are not shown due to high number of genes",
                 ha="center", fontsize=12
             )
@@ -425,7 +425,7 @@ class generatePlots():
             axs.set_xticks(axs.get_xticks()[::2])
             axs.tick_params(axis='both', which='major', labelsize=10)
             plt.figtext(
-                0.5, 0.1,
+                0.505, 0.01,
                 "Some gene labels are not shown due to high number of genes",
                 ha="center", fontsize=12
             )
@@ -1096,6 +1096,11 @@ class generateReport():
         )
         calculate = calculateValues(self.threshold)
 
+        if args.panel_filters:
+            filter_str = self.panel_filters(filters=args.panel_filters)
+        else:
+            filter_str = ''
+
         # apply styling to tables for displaying in report
         sub_threshold_stats, low_exon_columns, gene_issues,\
             exon_issues = styling.style_sub_threshold()
@@ -1129,6 +1134,7 @@ class generateReport():
         report_vals["exon_issues"] = str(exon_issues)
         report_vals["build"] = build
         report_vals["panel"] = panel
+        report_vals["filter_str"] = filter_str
         report_vals["vcfs"] = vcfs
         report_vals["version"] = version
         report_vals["panel_pct_coverage"] = panel_pct_coverage
@@ -1150,6 +1156,30 @@ class generateReport():
 
         # write output report to file
         self.write_report(html_string, args.output)
+
+
+    def panel_filters(self, filters) -> str:
+        """
+        Generate HTML formatted filters for drop down menu for full
+        gene plots in the report
+
+        Parameters
+        ----------
+        filters : list
+            list of filters formatted as ['panel1:gene1,gene2', 'panel2:gene3,gene4']
+
+        Returns
+        -------
+        str
+            HTML string of filter options for drop down menu
+        """
+        filters = {x.split(':')[0]: x.split(':')[1] for x in filters}
+        filter_str = ""
+
+        for panel, genes in filters.items():
+            filter_str += f'<option value="{genes}">{panel}</option>'
+        
+        return filter_str
 
 
     def build_report(
@@ -1187,6 +1217,17 @@ class generateReport():
             alt="" style="vertical-align:middle; padding-bottom:3px">'.format(
             data_uri)
 
+        hide_filter = ""
+        hide_plots = ""
+
+        if all_plots == "":
+            # filter str is empty => no panel filters set for plots or
+            # no plots to show => hide the div with the dropdown
+            hide_plots = 'style="display:none;"'
+
+        if report_vals["filter_str"] == "":
+            hide_filter = 'style="display:none;"'
+
         t = Template(html_template)
 
         date = datetime.today().strftime('%Y-%m-%d')
@@ -1207,6 +1248,9 @@ class generateReport():
             low_cov_plots=fig,
             coverage_per_chromosome_fig=coverage_per_chromosome_fig,
             all_plots=all_plots,
+            panel_filters=report_vals["filter_str"],
+            hide_filter=hide_filter,
+            hide_plots=hide_plots,
             summary_plot=summary_plot,
             gene_stats=gene_stats,
             gene_table_headings=report_vals["gene_table_headings"],
@@ -1390,6 +1434,16 @@ def parse_args():
         required=False
     )
     parser.add_argument(
+        '--panel_filters', nargs='+',
+        help=(
+            'Preset filters of genes / transcripts to set for the full gene '
+            'plots, these will be presented in a drop down menu for filtering '
+            'the plots. These should be passed as key:value pairs of panel '
+            'name to display in the drop down and a comma separated list of '
+            'gene symbols to filter with (i.e. panel1:gene1,gene2,gene3...)'
+        )
+    )
+    parser.add_argument(
         '-m', '--summary',
         help="If passed, a short paragraph will be included in the\
         summary section. This includes details on the sequencing and the\
@@ -1504,10 +1558,10 @@ def main():
                 all_plots = pool.map(plots.all_gene_plots, split_dfs)
                 all_plots = "".join(all_plots)
     else:
-        all_plots = "<br><b>Full gene plots have been omitted from this report\
-            due to the high number of genes in the panel.</b></br>"
+        # not generating plots => set to empty string to hide section in report
+        all_plots = ""
 
-    if len(low_raw_cov.index) > 0:
+    if len(low_raw_cov.index) > 1000000:
         # some low covered regions, generate plots
         print("Generating plots of low covered regions")
 
