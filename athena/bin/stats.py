d42b7ca4eeb1c873000ddf57a90457353ca55018
Jethro Rainford <rainford1995@gmail.com>
2023-04-14 10:33:15 +0100

    futher optimise calculating thrshold values

--- athena/bin/stats.py
+++ athena/bin/stats.py
@@ -37,41 +37,40 @@ class stats():
         pd.DataFrame
             dataframe of transcript level coverage values
         """
-        gene_coverage, exon_coverage = self.generate_empty_dfs(coverage_data)
+        exon_coverage = self._generate_empty_df(
+            per_base_data=coverage_data,
+            columns=['chrom', 'exon_start', 'exon_end', 'gene', 'transcript', 'exon']
+        )
         coverage_data = unbin(coverage_data)
 
         # calculate required exon level stats
-        exon_coverage = self.calculate_minimum(
+        exon_coverage = self._calculate_minimum(
             data=coverage_data,
             output=exon_coverage,
-            index=['tx', 'exon'],
+            index=['transcript', 'exon'],
             column='cov'
         )
 
-        exon_coverage = self.calculate_mean(
+        exon_coverage = self._calculate_mean(
             data=coverage_data,
             output=exon_coverage,
-            index=['tx', 'exon'],
+            index=['transcript', 'exon']
+        )
+
+        exon_coverage = self._calculate_maximum(
+            data=coverage_data,
+            output=exon_coverage,
+            index=['transcript', 'exon'],
             column='cov'
         )
 
-        exon_coverage = self.calculate_maximum(
+        exon_coverage = self._calculate_exon_thresholds(
             data=coverage_data,
             output=exon_coverage,
-            index=['tx', 'exon'],
-            column='cov'
-        )
-
-        exon_coverage = self.calculate_exon_thresholds(
-            data=coverage_data,
-            output=exon_coverage,
-            index=['tx', 'exon'],
             thresholds=thresholds
         )
 
         return exon_coverage
-
-           # get list of transcripts in data
 
 
     def calculate_exon_stats_parallel(self, coverage_data, thresholds) -> pd.DataFrame:
@@ -93,11 +92,11 @@ class stats():
         """
         # split per base dataframe into an array of dataframes by transcript
         # for passing to calculate_exon_stats() in parrallel
-        transcripts = sorted(coverage_data.tx.unique().tolist())
-        tx_array = np.array_split(np.array(transcripts), multiprocessing.cpu_count())
+        transcripts = sorted(coverage_data.transcript.unique().tolist())
+        transcript_array = np.array_split(np.array(transcripts), multiprocessing.cpu_count())
         split_dfs = np.asanyarray(
-            [coverage_data[coverage_data["tx"].isin(x)]
-            for x in tx_array], dtype=object
+            [coverage_data[coverage_data["transcript"].isin(x)]
+            for x in transcript_array], dtype=object
         )
 
         with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
@@ -119,41 +118,96 @@ class stats():
         exon_stats.exon = exon_stats.exon.astype('category')
         exon_stats.exon.cat.reorder_categories(
             natsorted(set(exon_stats.exon)), inplace=True, ordered=True)
-        exon_stats.sort_values(by=['gene', 'tx', 'exon'], inplace=True)
+        exon_stats.sort_values(by=['gene', 'transcript', 'exon'], inplace=True)
+        exon_stats.reset_index(drop=True, inplace=True)
 
         return exon_stats
 
 
-    def generate_empty_dfs(self, per_base_data):
+    def calculate_gene_stats(self, coverage_data, exon_data, thresholds) -> pd.DataFrame:
         """
-        Generate exon and gene level dataframes for adding values to,
-        based off columns in given per base dataframe, keeping just
-        single rows for each unique region from the per base rows.
+        Calculates per gene coverage stats from the per exon stats
+
+        Parameters
+        ----------
+        coverage_data : pd.DataFrame
+            dataframe of per base coverage data
+        exon_data : pd.DataFrame
+            dataframe of per exon coverage stats
+        thresholds : list
+            list of thresholds at which to calculate coverage
+
+        Returns
+        -------
+        pd.DataFrame
+            dataframe of per gene coverage stats
+        """
+        print('Calculating gene stats')
+        print(exon_data)
+        gene_coverage = self._generate_empty_df(
+            per_base_data=exon_data,
+            columns=['chrom', 'gene', 'transcript']
+        )
+
+        gene_coverage = self._calculate_minimum(
+            data=exon_data,
+            output=gene_coverage,
+            index=['transcript'],
+            column='min'
+        )
+
+        gene_coverage = self._calculate_mean(
+            data=coverage_data,
+            output=gene_coverage,
+            index=['transcript']
+        )
+
+        gene_coverage = self._calculate_maximum(
+            data=exon_data,
+            output=gene_coverage,
+            index=['transcript'],
+            column='max'
+        )
+
+        print('exon_stats before calculating thresholds for genes')
+        print(exon_data)
+
+        gene_coverage = self._calculate_gene_thresholds(
+            data=exon_data,
+            output=gene_coverage,
+            thresholds=thresholds
+        )
+
+        print('finished calculating gene stats')
+        print('exon stats:\n', exon_data)
+        print('gene stats:\n', gene_coverage)
+
+        return gene_coverage
+
+
+    def _generate_empty_df(self, per_base_data, columns) -> pd.DataFrame:
+        """
+        Generate empty dataframes for adding values to, based off columns
+        in given per base dataframe, keeping just one row per unique region
+        from the per base rows.
 
         Parameters
         ----------
         per_base_data : pd.DataFrame
             dataframe of per base coverage data to extract unique exon
             and gene rows from
+        columns : list
+            columns to select from per_base_data
         
         Returns
         -------
-        gene_coverage : pd.DataFrame
-            dataframe of unique gene rows
-        exon_coverage : pd.DataFrame
-            dataframe of unique exon rows
+        pd.DataFrame
+            dataframe of unique rows for the given columns
         """
-        gene_coverage = per_base_data[[
-            'chrom', 'gene', 'tx'
-        ]].drop_duplicates().reset_index(drop=True)
-        exon_coverage = per_base_data[[
-            'chrom', 'exon_start', 'exon_end', 'gene', 'tx', 'exon'
-        ]].drop_duplicates().reset_index(drop=True)
-
-        return gene_coverage, exon_coverage
+        return per_base_data[columns].drop_duplicates().reset_index(drop=True)
 
 
-    def calculate_minimum(self, data, output, index, column):
+    def _calculate_minimum(self, data, output, index, column):
         """
         Calculates the minimum coverage for each region
 
@@ -173,7 +227,7 @@ class stats():
         pd.DataFrame
             output dataframe with min column appended
         """
-        minimum = data.groupby(index)[column].min().reset_index()
+        minimum = data.groupby(index, as_index=False)[column].min()
         minimum.rename(columns={column: 'min'}, inplace=True)
 
         output = pd.merge(output, minimum, on=index, validate='1:1')
@@ -181,7 +235,7 @@ class stats():
         return output
 
 
-    def calculate_maximum(self, data, output, index, column) -> pd.DataFrame:
+    def _calculate_maximum(self, data, output, index, column) -> pd.DataFrame:
         """
         Calculates the maximum coverage for each region
 
@@ -201,13 +255,13 @@ class stats():
         pd.DataFrame
             output dataframe with max column appended
         """
-        maximum = data.groupby(index)[column].max().reset_index()
+        maximum = data.groupby(index, as_index=False)[column].max()
         maximum.rename(columns={column: 'max'}, inplace=True)
 
         return pd.merge(output, maximum, on=index, validate='1:1')
 
 
-    def calculate_mean(self, data, output, index, column) -> pd.DataFrame:
+    def _calculate_mean(self, data, output, index) -> pd.DataFrame:
         """
         Calculates the mean coverage for each region
 
@@ -219,33 +273,35 @@ class stats():
             dataframe to which to add the calculated values to
         index : list
             column names to use as index for calculating against
-        column : str
-            column from which to calculate
         
         Returns
         -------
         pd.DataFrame
             output dataframe with mean column appended
         """
-        mean = data.groupby(index)[column].mean().reset_index()
-        mean.rename(columns={column: 'mean'}, inplace=True)
+        mean = data.groupby(index, as_index=False)['cov'].mean()
+        mean.rename(columns={'cov': 'mean'}, inplace=True)
 
         return pd.merge(output, mean, on=index, validate='1:1')   
 
 
-    def calculate_exon_thresholds(self, data, output, index, thresholds
+    def _calculate_exon_thresholds(self, data, output, thresholds
         ) -> pd.DataFrame:
         """
-        Calculates the percent coverage at each given threshold for each region
+        Calculates the percent coverage at each given threshold for each exon.
+
+        This is calculated as:
+
+                        total bases > threshold
+                        -----------------------  X 100 
+                            exon length
 
         Parameters
         ----------
         data : pd.DataFrame
-            dataframe of from which calculate values
+            dataframe of per base coverage values from which to calculate
         output : pd.DataFrame
             dataframe to which to add the calculated values to
-        index : list
-            column names to use as index for calculating against
         thresholds : list
             list of thresholds at which to calculate coverage
         
@@ -254,56 +310,105 @@ class stats():
         pd.DataFrame
             output dataframe with threshold columns appended
         """
-        def calculate_pct(data, threshold) -> int:
-            """
-            Calculates the percent coverage at the threshold for the
-            given subset of data (i.e. the exon of each transcript)
-
-            Parameters
-            ----------
-            data : pd.DataFrame
-                subset of dataframe to calculate percent coverage at threshold
-            threshold : int
-                threshold to calculte coverage at
-
-            Returns
-            -------
-            int
-                percent coverage for the region
-            """
-            if isinstance(data, pd.core.frame.DataFrame):
-                exon_length = int(data.iloc[0].exon_end) - \
-                    int(data.iloc[0].exon_start)
-                above_threshold = len(
-                    data.iloc[np.where(data['cov'] > threshold)].index)
-
-                return (above_threshold / exon_length) * 100
+        data['exon_length'] = data['exon_end'] - data['exon_start']
 
         for threshold in thresholds:
-            # numpy vectorised call of the above calculate_pct() function
-            # for much faster calculation on large dataframes vs iterating
-            threshold_coverage = np.vectorize(calculate_pct, otypes=[object])(
-                data.groupby(index)[['cov', 'exon_start', 'exon_end']],
-                threshold,
-            )
+            # calculate the total bases above the given threshold
+            # force exon length to be returned in output dataframe by
+            # selecting the max from the column (where all will be the
+            # same as this is being called on each exon)
+            over = data.groupby(['transcript', 'exon']).agg(**{
+                f"{threshold}x":
+                pd.NamedAgg(column="cov", aggfunc=lambda x: sum(x > threshold)),
+                'exon_length': pd.NamedAgg(column="exon_length", aggfunc=lambda x: max(x))
+            })
 
-            # join numpy series back to output dataframe
-            output = output.join(pd.DataFrame(
-                threshold_coverage, columns=['_', f"{threshold}x"]
-            )[f"{threshold}x"])
-        
-            output[f"{threshold}x"] = output[f"{threshold}x"].astype(float)
+            # calculate % at current threshold
+            over[f"{threshold}x"] = over[f"{threshold}x"] /\
+                 over['exon_length'] * 100
+
+            # add threshold values to output dataframe
+            output = pd.merge(
+                output, over[[f'{threshold}x']],
+                on=['transcript', 'exon'],
+                validate='1:1'
+            )
 
         return output
 
 
+    def _calculate_gene_thresholds(self, data, output, thresholds) -> pd.DataFrame:
+        """
+        Calculate threshold values at gene level from previously calculated
+        exon values, normalising against length of each exon.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            per exon data to calculate from
+        outut : pd.DataFrame
+            per gene dataframe to add gene threshold values to
+        thresholds : list
+            list of thresholds at which to calculate coverage
+            
+        Returns
+        -------
+        pd.DataFrame
+            dataframe of per gene values with thresholds appended
+        """
+        data['exon_length'] = data['exon_end'] - data['exon_start']
+
+        thresholds = [f"{threshold}x" for threshold in thresholds]
+
+        # calculate gene length as the sum of exon lengths for each
+        # transcript, then map back onto original dataframe
+        gene_length = data.groupby(['transcript'], as_index=False)['exon_length'].sum()
+        gene_length.rename(columns={'exon_length': 'gene_length'}, inplace=True)
+        data = pd.merge(data, gene_length, on='transcript', validate='m:1')
+
+        for threshold in thresholds:
+            # calculate the fraction each exon contributes to
+            # overall gene coverage
+            data[threshold] = data[threshold] * data['exon_length'] / data['gene_length']
+
+
+        # sum each of the exon coverage fractions for each transcript to
+        # get total percent coverage
+        data = data.groupby(['transcript']).agg({x: "sum" for x in thresholds})
+
+        return pd.merge(output, data, on='transcript', validate='1:1')
+
 
 if __name__=="__main__":
-    data = pd.read_csv(sys.argv[1], sep='\t',
-                     names=["chrom", "exon_start", "exon_end", "gene", "tx", "exon",
-            "cov_start", "cov_end", "cov"],
-            dtype={'chrom': str, 'exon_start': int, 'exon_end': int, 'gene': str, 'tx': str, 'exon': str, 'cov_start': int, 'cov_end': int, 'cov': int})
+    data = pd.read_csv(
+        sys.argv[1], sep='\t',
+        names=[
+            "chrom", "exon_start", "exon_end", "gene", "transcript",
+            "exon", "cov_start", "cov_end", "cov"
+        ],
+        dtype={
+        'chrom': str, 'exon_start': int, 'exon_end': int, 'gene': str,
+        'transcript': str, 'exon': str, 'cov_start': int,
+        'cov_end': int, 'cov': int
+    })
 
 
-    stats().calculate_exon_stats_parallel(data, thresholds=[100, 500, 1000, 1500, 2000])
+    thresholds = [10, 50, 100, 150, 200]
+
+    exon_stats = stats().calculate_exon_stats_parallel(
+        data, thresholds=thresholds)
+
+    print('finished calculating exon stats:')
+    print(exon_stats)
+
+    gene_stats = stats().calculate_gene_stats(data, exon_stats, thresholds)
+
+    print('completed all calculations')
+    print('exon stats:')
+    print(exon_stats)
+    print('gene stats')
+    print(gene_stats)
+
+    # print('here3')
+    # print(gene_stats)
 
