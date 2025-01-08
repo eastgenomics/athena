e97a26ebb5da88df5d430eb2293e0f6f10014ae1
Jethro Rainford <rainford1995@gmail.com>
2025-01-08 11:22:34 +0000

    delete commented lines

--- athena/tests/__init__.py
+++ athena/tests/__init__.py
@@ -2,11 +2,5 @@ import sys
 from pathlib import Path
 
 TEST_DATA_DIR = Path(__file__).absolute().parent.joinpath("test_data")
-# sys.path.append(TEST_DATA_DIR)
-
-# add in parent athena dir to path for importing
-# sys.path.append(
-#     os.path.abspath(os.path.join(os.path.realpath(__file__), "../../"))
-# )
 
 sys.path.append(Path(__file__).absolute().parent.parent)
