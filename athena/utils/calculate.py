ec732f4f42b4815328b00acf83ed6066735faa97
Jethro Rainford <rainford1995@gmail.com>
2025-01-10 10:37:39 +0000

    fix type hint

--- athena/utils/calculate.py
+++ athena/utils/calculate.py
@@ -217,7 +217,7 @@ def pct_thresholds(
 
 
 def multi_sample_mean_and_std_dev(
-    sample_dfs: List[pl.DataFrame, pl.DataFrame],
+    sample_dfs: List[Tuple[pl.DataFrame, pl.DataFrame]],
 ) -> pl.DataFrame:
     """
     Calculates the normalised mean and std deviation across all positions.
