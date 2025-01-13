49fe8b631d2ee64001bf8af74e0dfa31c4bd0259
Jethro Rainford <rainford1995@gmail.com>
2025-01-13 09:18:14 +0000

    add new column types to constants

--- athena/utils/constants.py
+++ athena/utils/constants.py
@@ -12,6 +12,9 @@ DATAFRAME_TYPES = {
     "depth_bin_start": pl.UInt32,
     "depth_bin_end": pl.UInt32,
     "depth": pl.UInt32,
+    "position": pl.UInt32,
+    "mean": pl.Float64,
+    "std": pl.Float64,
 }
 
 # value to use for normalising against for multi sample calculations
