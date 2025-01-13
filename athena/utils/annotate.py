2fb0a9132177694b58a832d215fcb51fc2f872de
Jethro Rainford <rainford1995@gmail.com>
2025-01-13 11:11:01 +0000

    add check for non zero exit code from bedtools intersetc

--- athena/utils/annotate.py
+++ athena/utils/annotate.py
@@ -48,6 +48,8 @@ def call_bedtools_intersect(
         Raised when a file with the proposed filename already exists
     subprocess.CalledProcessError
         Raised if a non-zero exit code returned from subprocess.run
+    RuntimeError
+        Raised if any error emitted to stderr from bedtools intersect call
     """
     log_handle.debug(
         "Annotating regions bed file via bedtools intersect with coverage data"
@@ -85,12 +87,15 @@ def call_bedtools_intersect(
     try:
         # TODO - add some check of contigs from the bed and genome file
         # to drop using the -sorted arg if they mismatch
-        subprocess.run(
-            f"bedtools intersect -sorted -nonamecheck -g {genome} -wa -wb -a"
-            f" {regions} -b {coverage} | cut -f7 --complement | gzip >"
+        proc = subprocess.run(
+            f"bedtools intersect -sorted -nonamecheck  -wa -wb -a {regions} -b"
+            f" {coverage} -g {genome} | cut -f7 --complement | gzip >"
             f" {output_file}",
             shell=True,
             check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+            text=True,
         )
     except subprocess.CalledProcessError as err:
         raise subprocess.CalledProcessError(
@@ -101,6 +106,11 @@ def call_bedtools_intersect(
                 f"Error in calling bedtools intersect: {err.stderr.decode()}"
             ),
         ) from err
+
+    if "error" in proc.stderr.lower():
+        raise RuntimeError(
+            f"Error in calling bedtools intersect: {proc.stderr}"
+        )
 
     log_handle.debug(
         "Annotated regions bed file completed in %s, written to %s",
