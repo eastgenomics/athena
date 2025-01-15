c3988fc05015e07a0914bffadbf02e45f1099fa6
Jethro Rainford <rainford1995@gmail.com>
2025-01-15 15:02:50 +0000

    set limit from dx app input

--- dnanexus/src/eggd_athena.sh
+++ dnanexus/src/eggd_athena.sh
@@ -34,6 +34,15 @@ _set_string_inputs() {
     [ -n "$indication" ] && indication="--indication '${indication}' " || unset indication
 }
 
+_set_integer_inputs() {
+    : '''
+    Format integer inputs correctly to be passable as inputs.
+
+    If not specified will be unset, this allows them to be implicitly skipped.
+    '''
+    [ -n "$limit" ] && limit="--limit $limit " || unset limit
+}
+
 _set_optional_file_inputs() {
     : '''
     Format optional file inputs correctly
@@ -75,6 +84,7 @@ main() {
 
     _set_bool_inputs
     _set_string_inputs
+    _set_integer_inputs
 
     python3 athena/athena.py report \
         --regions "$regions_path" \
@@ -91,6 +101,7 @@ main() {
         $panel_filters \
         $summary \
         $summary_file \
+        $limit \
         $plot_chromosomes \
         $plot_sub_threshold \
         $write_data
