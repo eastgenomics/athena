526a7852aac2fdb165db902351df737cbb9248cd
Jethro Rainford <rainford1995@gmail.com>
2025-01-17 14:59:04 +0000

    add test boilerplate

---
+++ tests/test_calculate.py
@@ -0,0 +1,34 @@
+from unittest.mock import patch
+
+import pytest
+
+from athena.utils import calculate
+from tests import TEST_DATA_DIR
+
+
+class TestRegionCoverage:
+    pass
+
+
+class TestTotalPctCoverage:
+    pass
+
+
+class TestMinMeanMax:
+    pass
+
+
+class TestPctThresholds:
+    pass
+
+
+class TestCalculateNormalisationFactor:
+    pass
+
+
+class TestMultiSampleMeanAndStdDev:
+    pass
+
+
+class TestNormaliseToSample:
+    pass
