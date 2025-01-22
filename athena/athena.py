ed08370dc604e59bdac8aac54033a02fc4ce836a
Jethro Rainford <rainford1995@gmail.com>
2025-01-22 12:43:21 +0000

    default to output report in cwd

--- athena/athena.py
+++ athena/athena.py
@@ -1,6 +1,7 @@
 """Main entrypoint to control all running of Athena"""
 
 import argparse
+from pathlib import Path
 from timeit import default_timer as timer
 
 from utils import calculate
@@ -123,7 +124,7 @@ def generate_report(args: argparse.Namespace) -> None:
         summary_plot=summary_plot,
         chromosome_plots=chromosome_plots,
         threshold=args.minimum,
-        sample=args.output,
+        sample=Path(args.output).name,
         build=args.build,
         panel=args.panel,
         panel_coverage_pct=panel_coverage_pct,
