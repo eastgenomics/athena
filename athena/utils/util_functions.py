b077d1111bd5d058dddfa46aba313c1066644b5d
Jethro Rainford <rainford1995@gmail.com>
2025-01-22 09:19:05 +0000

    add bonus log message

--- athena/utils/util_functions.py
+++ athena/utils/util_functions.py
@@ -392,6 +392,7 @@ def natsort(dataframe: pl.DataFrame, columns: tuple) -> pl.DataFrame:
         Raised when provided columns to sort by are not present in the df
     """
     if dataframe.height == 0:
+        log_handle.debug("Provided empty dataframe, nothing to sort...")
         return dataframe
 
     if any([x not in dataframe.columns for x in columns]):
