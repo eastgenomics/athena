04f7a19f0ac01134fb740fb1a2b1d5e9d5570baa
Jethro Rainford <rainford1995@gmail.com>
2025-01-22 09:18:06 +0000

    add early return to natsort when provided empty df

--- athena/utils/util_functions.py
+++ athena/utils/util_functions.py
@@ -391,6 +391,9 @@ def natsort(dataframe: pl.DataFrame, columns: tuple) -> pl.DataFrame:
     ValueError
         Raised when provided columns to sort by are not present in the df
     """
+    if dataframe.height == 0:
+        return dataframe
+
     if any([x not in dataframe.columns for x in columns]):
         raise ValueError(
             "Provided column(s) not present in available dataframe columns:"
