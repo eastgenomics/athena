3ad56f0e082e4f3d86220631d83ae8596cc0af76
Jethro Rainford <rainford1995@gmail.com>
2025-01-28 15:16:34 +0000

    fix tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -277,11 +277,12 @@ class TestWriteMultiSampleCoverage:
             {
                 "chrom": ["1", "1", "1", "1"],
                 "position": [10000, 10001, 10002, 10003],
-                "mean": [14.123, 16.262, 12.222, 13.333],
-                "std": [1.112, 1.545, 1.234, 1.443],
+                "obs_mean": [14.123, 16.262, 12.222, 13.333],
+                "obs_min": [10.10, 9.16, 11.89, 8.00],
+                "obs_max": [18.81, 16.76, 19.11, 18.01],
             },
             schema=util_functions.get_column_dtypes(
-                ["chrom", "position", "mean", "std"]
+                ["chrom", "position", "obs_mean", "obs_min", "obs_max"]
             ),
         )
         test_file = Path(tmp_path).joinpath("test_normal_coverage.tsv.gz")
